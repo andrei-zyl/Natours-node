b824014a49b344bc37122e7d31b64d769580720a
Alex <anzyl1986@gmail.com>
2021-02-15 21:21:03 +0300

    Added middleware for tourShema, getTourStats and getMonthlyPlan controllers

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name!'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'Tour name must have 40 or less characters!'],
+        minlength: [10, 'Tour name must have 10 or more characters!'],
+        validate: [validator.isAlpha, 'Tour name must contain only characters!']
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration!']
@@ -17,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty!']
+        required: [true, 'A tour must have a difficulty!'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        max: [5, 'Rating must be equal or below 5'],
+        min: [1, 'Rating must be equal or above 1']
     },
     ratingsQuantity: {
         type: Number,
@@ -31,7 +43,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price!']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val){
+                return val < this.price
+            },
+            message: 'Discount price ({VALUE}) should be below regular price!'
+        } 
+    },
     summary: {
         type: String,
         trim: true,
@@ -51,8 +71,35 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true},
 })
+
+tourSchema.virtual('durationWeeks').get(function(){
+    return this.duration / 7
+})
+
+tourSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+tourSchema.pre(/^find/, function(next){
+    this.find({secretTour: {$ne: true}})
+    next();
+});
+
+tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}})
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,15 +5,18 @@ const router = express.Router();
 
 // router.param('id', tourController.checkId);
 router.route('/top-5-cheap')
-    .get(tourController.aliasTours, tourController.getAllTours)
+    .get(tourController.aliasTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/')
     .get(tourController.getAllTours)
-    .post(/* tourController.checkBody, */ tourController.createTour)
+    .post(/* tourController.checkBody, */ tourController.createTour);
 
 router.route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(tourController.deleteTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
