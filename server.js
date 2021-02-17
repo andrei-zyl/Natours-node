f6e6b3d8c430c7eb2c064b29b09ade23f158131e
Alex <anzyl1986@gmail.com>
2021-02-17 15:08:09 +0300

    Implemented error handlers

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('Uncaught Exception!!! Shutting down...')
+    process.exit(1);
+})
+
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
- //console.log(process.env.NODE_ENV)
+//console.log(process.env.NODE_ENV)
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB,{
@@ -17,6 +24,14 @@ mongoose.connect(DB,{
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App is running on port ${port}...`)
 })
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('Unhandled Rejection!!! Shutting down...')
+    server.close(() => {
+        process.exit(1);
+    })
+});
