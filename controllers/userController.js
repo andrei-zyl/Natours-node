89c88454d0874e732a862e890d9bc6c168f9920d
Alex <anzyl1986@gmail.com>
2021-02-20 18:50:11 +0300

    created User Schema, implemented authentication and authotization with JWT as well as deleting tours according to the user's role

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route has not been defined!'
-    })
-}
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        status: 'success',
+        result: users.length,
+        data: {users}
+    });
+});
 
 exports.createUser = (req, res) => {
     res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
 router.route('/')
     .get(userController.getAllUsers)
-    .post(userController.createUser)
+    .post(userController.createUser);
 
 router.route('/:id')
     .get(userController.getUser)
     .patch(userController.updateUser)
-    .delete(userController.deleteUser)
+    .delete(userController.deleteUser);
 
 module.exports = router;
