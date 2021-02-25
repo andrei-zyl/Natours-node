8ae926ac49f146e5ebbf1977fea6650154b23308
Alex <anzyl1986@gmail.com>
2021-02-25 19:01:45 +0300

    Implemented Review model, as well as getAllReviews and createReview methods

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -10,7 +11,7 @@ const tourSchema = new mongoose.Schema({
         trim: true,
         maxlength: [40, 'Tour name must have 40 or less characters!'],
         minlength: [10, 'Tour name must have 10 or more characters!'],
-        validate: [validator.isAlpha, 'Tour name must contain only characters!']
+        //validate: [validator.isAlpha, 'Tour name must contain only characters!']
     },
     slug: String,
     duration: {
@@ -75,7 +76,34 @@ const tourSchema = new mongoose.Schema({
     secretTour: {
         type: Boolean,
         default: false
-    }
+    },
+    startLocation: {
+        type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+    },
+    locations: [{
+        type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+    }],
+    guides:[
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 },
 {
     toJSON: {virtuals: true},
@@ -91,13 +119,27 @@ tourSchema.pre('save', function(next){
     next();
 });
 
+// tourSchema.pre('save', async function(next){
+//     const guidesPromises = this.guides.map(async el => await User.findById(el));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
+
 tourSchema.pre(/^find/, function(next){
-    this.find({secretTour: {$ne: true}})
+    this.find({secretTour: {$ne: true}});
+    next();
+});
+
+tourSchema.pre(/^find/, function(next){
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    });
     next();
 });
 
 tourSchema.pre('aggregate', function(next){
-    this.pipeline().unshift({$match: {secretTour: {$ne: true}}})
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}});
     next();
 });
 
